63a9cd09a6405cf66c368c9c986721b7e9b5bc22
lol4meh <137432401+lol4meh@users.noreply.github.com>
2023-06-23 19:41:13 +0300

    Add files via upload

---
+++ source/PlayState.hx
@@ -0,0 +1,30 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.ui.FlxButton;
+import flixel.sound.FlxSound;
+
+class PlayState extends FlxState
+{
+	var volume:Float = 1;
+	override public function create()
+	{
+        super.create();
+
+		FlxG.sound.volume = volume;
+		FlxG.sound.muted = false;
+
+		var soundtest:FlxButton = new FlxButton(0, 0, "play jumpscare", playjumpscaresound);
+		soundtest.screenCenter();
+		add(soundtest);
+	}
+
+	function playjumpscaresound()
+	{
+		    FlxG.sound.play("assets/jumpscare.ogg");
+	}
+}
